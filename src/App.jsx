41d8868296c33ed65425f32f1a0de9089078e991
Yordanos-zegeye <yordanoszegeye24@gmail.com>
2024-10-11 12:36:01 +0300

    finished the hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react";
+import Hero from "./components/Hero";
+import Generator from "./components/Generator";
+import Workout from "./components/Workout";
 
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <main className="min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white text-sm sm:text-base">
+      <Hero />
+      <Generator />
+      <Workout />
+    </main>
+  );
 }
